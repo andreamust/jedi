1f18b95f807744a964d4cd448f53285eef5a6521
andreamust <andrea.poltronieri21@gmail.com>
2022-10-13 10:43:31 +0200

    reformat repository

--- jedi/resize_dataset.py
+++ jedi/resize_dataset.py
@@ -23,7 +23,7 @@ def resize_dataset(dataset_path: Path, output_path: str, num: int) -> None:
 
 if __name__ == '__main__':
     my_parser = argparse.ArgumentParser(prog='resize_dataset',
-                                        description='List the content of a folder')
+                                        description='Content of a folder')
 
     my_parser.add_argument('--dataset_path', help='a first argument')
     my_parser.add_argument('--output_path', help='a second argument')
@@ -31,4 +31,5 @@ if __name__ == '__main__':
 
     args = my_parser.parse_args()
 
-    resize_dataset(dataset_path=args.dataset_path, output_path=args.output_path, num=args.n)
+    resize_dataset(dataset_path=args.dataset_path, output_path=args.output_path,
+                   num=args.n)
